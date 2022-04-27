48a8f17f489220926f75917a4461d75e8d466999
Jesus David Posada Escobar <jesus.posada@wiedii.co>
2022-04-27 13:48:15 -0500

    168331 update name database

--- src/src/app.js
+++ src/src/app.js
@@ -1,11 +1,11 @@
-var apm = require('elastic-apm-node').start({
-serviceName: 'Node_Prototype_Mongo',
-secretToken: '',
-serverUrl: 'https://elastic.wiedii.co:8200',
-environment: 'develop'
+var apm = require("elastic-apm-node").start({
+  serviceName: "Node_Prototype_Mongo",
+  secretToken: "",
+  serverUrl: "https://elastic.wiedii.co:8200",
+  environment: "develop",
 });
-require('dotenv').config();
-const Server = require('./models/server.models');
+require("dotenv").config();
+const Server = require("./models/server.models");
 
 const server = new Server();
 
