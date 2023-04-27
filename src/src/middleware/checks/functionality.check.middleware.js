4ea1adc98b39b9680910d5cc70531d155bd3c1af
Fabio Guerrero <fabio.guerrero@wiedii.co>
2023-04-27 14:07:41 -0500

    236016 custon validator for existing functionalities

--- src/src/middleware/checks/functionality.check.middleware.js
+++ src/src/middleware/checks/functionality.check.middleware.js
@@ -18,7 +18,10 @@ module.exports = {
         // TODO: validate role,
         check('name')
             .isString().withMessage(new ErrorModel(errorsConst.functionalityErrors.nameRequired)).bail()
-            .isLength({ min: 1, max: 10 }).withMessage(new ErrorModel(errorsConst.functionalityErrors.nameInvalid)),
+            .isLength({ min: 1, max: 10 }).withMessage(new ErrorModel(errorsConst.functionalityErrors.nameInvalid))
+            .custom((name, { req }) => functionalityValidators.validateFunctionality({ name }, req)),
+        sharedValidators.validateError,
+        check('functionality', new ErrorModel(errorsConst.functionalityErrors.nameAlreadyExists)).custom((value) => !value),
         sharedValidators.validateError,
     ],
 
