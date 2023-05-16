da0ab74d2a66c3b3789727ac442d90d4c0ecbda5
Jhoan Cardozo <jhoan.cardozo@wiedii.co>
2023-05-16 16:28:02 -0500

    239435 Adjustment of the user filter endpoint on the cotranscat project, change of parameter input and form of response to request.

--- src/src/controllers/user/user.controller.js
+++ src/src/controllers/user/user.controller.js
@@ -9,7 +9,10 @@ const {
 
 // Libraries
 const unicodeNormalizerLibrary = require('unorm')
-const { col, fn, where } = require("sequelize");
+const { col, fn, where, Op } = require("sequelize");
+
+// Models - model
+const { IndicativeNumber, DocumentType, Client, Municipality, Department } = require("../../models/index.models");
 
 // Models - Queries
 const { userQuery } = require("../../models/index.queries");
@@ -36,43 +39,121 @@ module.exports = {
   },
   searchUsers: async (req, res) => {
 
-    const { name, lastName, numberDocument } = req.body;
+    let { filterValue } = req.body;
 
-    if (!name && !lastName && !numberDocument) return responseHelpers.responseSuccess(res, [])
-
-    const nameWithoutTildes = name ? unicodeNormalizerLibrary.nfd(name).replace(/[\u0300-\u036f]/g, "") : null;
-    const lastNameWithoutTildes = lastName ? unicodeNormalizerLibrary.nfd(lastName).replace(/[\u0300-\u036f]/g, "") : null;
+    if (isNaN(filterValue)) 
+      filterValue = filterValue ? (unicodeNormalizerLibrary.nfd(filterValue).replace(/[\u0300-\u036f]/g, "")).toLowerCase() : null;
 
     try {
       const usersFound = await userQuery.findUserQuery({
         where: {
-          ...(name && {
-            name: where(
-              fn('LOWER', col('name')),
-              'LIKE',
-              `%${nameWithoutTildes.toLowerCase()}%`
-            )
-          }),
-          ...(lastName && {
-            lastName: where(
-              fn('LOWER', col('lastName')),
-              'LIKE',
-              `%${lastNameWithoutTildes.toLowerCase()}%`
-            )
-          }),
-          ...(numberDocument && { numberDocument })
+          [Op.or]: [
+            {
+              name: where(
+                fn('LOWER', col('User.name')),
+                'LIKE',
+                `%${filterValue}%`
+              )
+            },
+            {
+              lastName: where(
+                fn('LOWER', col('User.lastName')),
+                'LIKE',
+                `%${filterValue}%`
+              )
+            },
+            {
+              numberDocument: {
+                [Op.like]: `%${filterValue}%`
+              }
+            }
+          ]
         },
-        include: []
+        include: [
+          {
+            model: IndicativeNumber,
+            as: "UserIndicativePhone"
+          },
+          {
+            model: DocumentType,
+            as: "UserDocumentType"
+          },
+          {
+            model: Client,
+            as: "UserClient",
+            required: false,
+            include: [
+              {
+                model: IndicativeNumber,
+                as: "ClientIndicativeNumberWhatsApp",
+                required: false
+              },
+              {
+                model: Municipality,
+                as: "ClientMunicipality",
+                required: false,
+                include: [
+                  {
+                    model: Department,
+                    as: "MunicipalityDepartment",
+                    required: false
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       })
-
       const users = usersFound.map(
         ({
-          id,
+          id, numberDocument, name, lastName, UserDocumentType, UserIndicativePhone, numberPhone, 
+          UserClient :{numberPhoneWhatsapp, email, address, ClientIndicativeNumberWhatsApp, ClientMunicipality },
           ...user
-        }) => ({
-          id: sharedHelpers.encryptIdDataBase(id),
-          ...user,
-        })
+        }) => {
+
+          let municipality = null
+          let department = null
+
+          if (ClientMunicipality.id) {
+            const { id: idMunicipality, name, MunicipalityDepartment: departmentData, ...otherDataMunicipality } = ClientMunicipality
+            municipality = {
+              name,
+              id: sharedHelpers.encryptIdDataBase(idMunicipality),
+            }
+            if (departmentData.id) {
+
+              department = {
+                name: departmentData.name,
+                id: sharedHelpers.encryptIdDataBase(departmentData.id),
+              }
+            }
+          }
+
+          return {
+            id: sharedHelpers.encryptIdDataBase(id),
+            numberDocument, 
+            name, 
+            lastName,
+            documentType: {
+              ...UserDocumentType,
+              id: sharedHelpers.encryptIdDataBase(UserDocumentType.id),
+            },
+            indicativeNumber: {
+              ...UserIndicativePhone,
+              id: sharedHelpers.encryptIdDataBase(UserIndicativePhone.id),
+            },
+            numberPhone,
+            email: email ?? undefined,
+            address: address ?? undefined,
+            numberPhoneWhatsapp: numberPhoneWhatsapp ?? undefined,
+            indicativeNumberWhatsApp: {
+              ...ClientIndicativeNumberWhatsApp,
+              id: ClientIndicativeNumberWhatsApp.id ? sharedHelpers.encryptIdDataBase(ClientIndicativeNumberWhatsApp.id) : null,
+            },
+            municipality,
+            department
+          }
+        }
       )
 
       return responseHelpers.responseSuccess(res, users)
