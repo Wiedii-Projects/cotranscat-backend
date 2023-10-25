8389276ba6e805605e6c237f470c93df451e55d7
Jhoan Cardozo <jhoan.cardozo@wiedii.co>
2023-10-25 16:07:19 -0500

    270363 Required to perform Configuration of connection environment variables for webSocket project for cotranscat back-end project

--- src/src/socket/index.js
+++ src/src/socket/index.js
@@ -1,11 +1,15 @@
 
+// Constants
+const { domainWebSocket } = require('../constants/core/core-configurations.const');
+
+// Models
 const {SocketConnection} = require('./../socket/models/socket.model');
 
 const socketConnection = SocketConnection.getInstance();
 
 module.exports = {
     connectWebSocket: () => {
-        const url = 'http://192.168.50.10:7001/passage';
+        const url = `${domainWebSocket}/passage`
         socketConnection.connect(url);
     }
 }
